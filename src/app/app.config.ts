aedabe2bc14c1f2dc752622c7d26531d10acc30b
csonfack <sonfackyann@gmail.com>
2024-08-17 23:18:29 +0100

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,10 +7,12 @@ import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { CourseService } from '../services/course.service';
 import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimations(), // required animations
-    provideToastr(),CourseService,
+    provideToastr(), CourseService,
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
   ]
 };
